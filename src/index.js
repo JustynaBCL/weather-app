c13b89dbf8589b86dd19dedfe0089c89633d24fd
JustynaBCL <85199931+JustynaBCL@users.noreply.github.com>
2021-07-25 16:44:33 +0100

    Added html file and src folder

---
+++ src/index.js
@@ -0,0 +1,34 @@
+let now = new Date();
+
+let currentDate = document.querySelector("#current-time");
+
+let date = now.getDate();
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+
+currentDate.innerHTML = `${day}, ${hour}:${minutes}`;
+
+function search(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city");
+  let cityInput = document.querySelector("#city-input");
+  cityElement.innerHTML = cityInput.value;
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
